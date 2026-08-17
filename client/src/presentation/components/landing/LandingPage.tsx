2e05d44cc7d3b97fe5cee5a717f1458f0cf0c62c
adnanemir04 <emirkirimis5101@gmail.com>
2026-08-17 16:27:12 +0300

    chore: revert quote transition interval to 8 seconds

--- client/src/presentation/components/landing/LandingPage.tsx
+++ client/src/presentation/components/landing/LandingPage.tsx
@@ -317,7 +317,7 @@ function QuoteCarousel() {
   React.useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % quotes.length);
-    }, 15000); // 15 seconds interval
+    }, 8000); // 8 seconds interval
     return () => clearInterval(interval);
   }, []);
 
